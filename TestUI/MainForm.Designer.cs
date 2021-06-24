a50a89613f416e75d18c64422df322f6fbe60931
elperry-c <elperry.contact@gmail.com>
2021-06-24 20:59:10 +0900

    2021_06_24_20:59

--- TestUI/MainForm.Designer.cs
+++ TestUI/MainForm.Designer.cs
@@ -31,27 +31,319 @@ namespace TestUI
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.graph1 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.emgLabel = new System.Windows.Forms.Label();
+            this.fftLabel = new System.Windows.Forms.Label();
+            this.graph2 = new System.Windows.Forms.PictureBox();
+            this.emgAxisX = new System.Windows.Forms.Label();
+            this.fftAxisX = new System.Windows.Forms.Label();
+            this.predictionText1 = new System.Windows.Forms.Label();
+            this.predictionTextl2 = new System.Windows.Forms.Label();
+            this.predictionResultValue1 = new System.Windows.Forms.Label();
+            this.predictionResultValue2 = new System.Windows.Forms.Label();
+            this.blueLegendLabel1 = new System.Windows.Forms.Label();
+            this.yellowLegendLabel1 = new System.Windows.Forms.Label();
+            this.yellowLegendLabel2 = new System.Windows.Forms.Label();
+            this.blueLegendLabel2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.graph1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graph2)).BeginInit();
             this.SuspendLayout();
             // 
             // serialPort
             // 
             this.serialPort.BaudRate = 115200;
             // 
+            // graph1
+            // 
+            this.graph1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.graph1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.graph1.Location = new System.Drawing.Point(58, 341);
+            this.graph1.Name = "graph1";
+            this.graph1.Size = new System.Drawing.Size(576, 306);
+            this.graph1.TabIndex = 0;
+            this.graph1.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(57, 716);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(104, 45);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(367, 709);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(83, 48);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "OFF";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(207, 716);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(104, 45);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // emgLabel
+            // 
+            this.emgLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.emgLabel.AutoSize = true;
+            this.emgLabel.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.emgLabel.Location = new System.Drawing.Point(304, 290);
+            this.emgLabel.Name = "emgLabel";
+            this.emgLabel.Size = new System.Drawing.Size(95, 48);
+            this.emgLabel.TabIndex = 7;
+            this.emgLabel.Text = "EMG";
+            this.emgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fftLabel
+            // 
+            this.fftLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.fftLabel.AutoSize = true;
+            this.fftLabel.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.fftLabel.Location = new System.Drawing.Point(946, 290);
+            this.fftLabel.Name = "fftLabel";
+            this.fftLabel.Size = new System.Drawing.Size(75, 48);
+            this.fftLabel.TabIndex = 9;
+            this.fftLabel.Text = "FFT";
+            this.fftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // graph2
+            // 
+            this.graph2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.graph2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.graph2.Location = new System.Drawing.Point(695, 341);
+            this.graph2.Name = "graph2";
+            this.graph2.Size = new System.Drawing.Size(576, 306);
+            this.graph2.TabIndex = 8;
+            this.graph2.TabStop = false;
+            // 
+            // emgAxisX
+            // 
+            this.emgAxisX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.emgAxisX.AutoSize = true;
+            this.emgAxisX.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.emgAxisX.Location = new System.Drawing.Point(312, 650);
+            this.emgAxisX.Name = "emgAxisX";
+            this.emgAxisX.Size = new System.Drawing.Size(63, 32);
+            this.emgAxisX.TabIndex = 10;
+            this.emgAxisX.Text = "time";
+            this.emgAxisX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fftAxisX
+            // 
+            this.fftAxisX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.fftAxisX.AutoSize = true;
+            this.fftAxisX.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.fftAxisX.Location = new System.Drawing.Point(923, 650);
+            this.fftAxisX.Name = "fftAxisX";
+            this.fftAxisX.Size = new System.Drawing.Size(108, 32);
+            this.fftAxisX.TabIndex = 11;
+            this.fftAxisX.Text = "freqency";
+            this.fftAxisX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // predictionText1
+            // 
+            this.predictionText1.AutoSize = true;
+            this.predictionText1.Font = new System.Drawing.Font("Meiryo UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.predictionText1.Location = new System.Drawing.Point(40, 72);
+            this.predictionText1.Name = "predictionText1";
+            this.predictionText1.Size = new System.Drawing.Size(468, 61);
+            this.predictionText1.TabIndex = 12;
+            this.predictionText1.Text = "現在の筋肉痛レベル：";
+            this.predictionText1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // predictionTextl2
+            // 
+            this.predictionTextl2.AutoSize = true;
+            this.predictionTextl2.Font = new System.Drawing.Font("Meiryo UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.predictionTextl2.Location = new System.Drawing.Point(90, 180);
+            this.predictionTextl2.Name = "predictionTextl2";
+            this.predictionTextl2.Size = new System.Drawing.Size(418, 61);
+            this.predictionTextl2.TabIndex = 13;
+            this.predictionTextl2.Text = "筋肉痛が治るまで：";
+            this.predictionTextl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // predictionResultValue1
+            // 
+            this.predictionResultValue1.AutoSize = true;
+            this.predictionResultValue1.Font = new System.Drawing.Font("Meiryo UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.predictionResultValue1.Location = new System.Drawing.Point(514, 72);
+            this.predictionResultValue1.Name = "predictionResultValue1";
+            this.predictionResultValue1.Size = new System.Drawing.Size(87, 61);
+            this.predictionResultValue1.TabIndex = 14;
+            this.predictionResultValue1.Text = "**";
+            this.predictionResultValue1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.predictionResultValue1.Visible = false;
+            // 
+            // predictionResultValue2
+            // 
+            this.predictionResultValue2.AutoSize = true;
+            this.predictionResultValue2.Font = new System.Drawing.Font("Meiryo UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.predictionResultValue2.Location = new System.Drawing.Point(514, 180);
+            this.predictionResultValue2.Name = "predictionResultValue2";
+            this.predictionResultValue2.Size = new System.Drawing.Size(87, 61);
+            this.predictionResultValue2.TabIndex = 15;
+            this.predictionResultValue2.Text = "**";
+            this.predictionResultValue2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.predictionResultValue2.Visible = false;
+            // 
+            // blueLegendLabel1
+            // 
+            this.blueLegendLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.blueLegendLabel1.AutoSize = true;
+            this.blueLegendLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.blueLegendLabel1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLegendLabel1.ForeColor = System.Drawing.Color.White;
+            this.blueLegendLabel1.Location = new System.Drawing.Point(527, 341);
+            this.blueLegendLabel1.Name = "blueLegendLabel1";
+            this.blueLegendLabel1.Size = new System.Drawing.Size(92, 46);
+            this.blueLegendLabel1.TabIndex = 16;
+            this.blueLegendLabel1.Text = "上腕";
+            this.blueLegendLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yellowLegendLabel1
+            // 
+            this.yellowLegendLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.yellowLegendLabel1.AutoSize = true;
+            this.yellowLegendLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.yellowLegendLabel1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLegendLabel1.ForeColor = System.Drawing.Color.Black;
+            this.yellowLegendLabel1.Location = new System.Drawing.Point(416, 341);
+            this.yellowLegendLabel1.Name = "yellowLegendLabel1";
+            this.yellowLegendLabel1.Size = new System.Drawing.Size(92, 46);
+            this.yellowLegendLabel1.TabIndex = 17;
+            this.yellowLegendLabel1.Text = "前腕";
+            this.yellowLegendLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yellowLegendLabel2
+            // 
+            this.yellowLegendLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.yellowLegendLabel2.AutoSize = true;
+            this.yellowLegendLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.yellowLegendLabel2.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLegendLabel2.ForeColor = System.Drawing.Color.Black;
+            this.yellowLegendLabel2.Location = new System.Drawing.Point(1052, 341);
+            this.yellowLegendLabel2.Name = "yellowLegendLabel2";
+            this.yellowLegendLabel2.Size = new System.Drawing.Size(92, 46);
+            this.yellowLegendLabel2.TabIndex = 18;
+            this.yellowLegendLabel2.Text = "前腕";
+            this.yellowLegendLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // blueLegendLabel2
+            // 
+            this.blueLegendLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.blueLegendLabel2.AutoSize = true;
+            this.blueLegendLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.blueLegendLabel2.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLegendLabel2.ForeColor = System.Drawing.Color.White;
+            this.blueLegendLabel2.Location = new System.Drawing.Point(1165, 341);
+            this.blueLegendLabel2.Name = "blueLegendLabel2";
+            this.blueLegendLabel2.Size = new System.Drawing.Size(92, 46);
+            this.blueLegendLabel2.TabIndex = 19;
+            this.blueLegendLabel2.Text = "上腕";
+            this.blueLegendLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(682, 650);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 32);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.Location = new System.Drawing.Point(1232, 650);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 32);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "255";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(908, 623);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(1324, 810);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.predictionResultValue2);
+            this.Controls.Add(this.predictionResultValue1);
+            this.Controls.Add(this.predictionTextl2);
+            this.Controls.Add(this.predictionText1);
+            this.Controls.Add(this.blueLegendLabel2);
+            this.Controls.Add(this.yellowLegendLabel2);
+            this.Controls.Add(this.yellowLegendLabel1);
+            this.Controls.Add(this.blueLegendLabel1);
+            this.Controls.Add(this.fftAxisX);
+            this.Controls.Add(this.emgAxisX);
+            this.Controls.Add(this.fftLabel);
+            this.Controls.Add(this.graph2);
+            this.Controls.Add(this.emgLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.graph1);
             this.Name = "MainForm";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "DOMSAP App";
+            ((System.ComponentModel.ISupportInitialize)(this.graph1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graph2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.PictureBox graph1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label emgLabel;
+        private System.Windows.Forms.Label fftLabel;
+        private System.Windows.Forms.PictureBox graph2;
+        private System.Windows.Forms.Label emgAxisX;
+        private System.Windows.Forms.Label fftAxisX;
+        private System.Windows.Forms.Label predictionText1;
+        private System.Windows.Forms.Label predictionTextl2;
+        private System.Windows.Forms.Label predictionResultValue1;
+        private System.Windows.Forms.Label predictionResultValue2;
+        private System.Windows.Forms.Label blueLegendLabel1;
+        private System.Windows.Forms.Label yellowLegendLabel1;
+        private System.Windows.Forms.Label yellowLegendLabel2;
+        private System.Windows.Forms.Label blueLegendLabel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TestUI/MainForm.cs
+++ TestUI/MainForm.cs
@@ -11,7 +11,11 @@ using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using MathNet.Numerics.Statistics; // NuGetパッケージの管理
+using System.Numerics;
+// [ツール]>>[NuGetパッケージマネージャ]>>[ソリューションの...管理]からMathNet.Numericsをインストール
+using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+using MathNet.Numerics.IntegralTransforms;
 
 namespace TestUI
 {
@@ -31,8 +35,9 @@ namespace TestUI
 
         DateTime dtNow = DateTime.Now;
 
-        ///////////////////////////////////////////////////////
-        //センサデータ取得
+
+        /// センサデータ取得
+        /// ////////////////////////////////////////////////
         delegate void setfocus();
         private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
@@ -87,7 +92,19 @@ namespace TestUI
                         emg_3chList.Add(emg_3ch);
                         emg_4chList.Add(emg_4ch);
 
-                        // グラフに表示
+                        // グラフ表示
+                        Invoke((setfocus)delegate ()
+                        {
+                            MakeGraphicsEMG((short)emg_1ch, (short)emg_2ch);
+                        });
+                        break;
+                    }
+                    else if (readdata[1] == 0x93) // 計測時刻応答
+                    {
+                        for (int i=2; i<15; i++)
+                        {
+
+                        }
                     }
                 }
             }
@@ -102,9 +119,141 @@ namespace TestUI
             }
         }
 
-            private void Form1_Load(object sender, EventArgs e)
+
+        /// ボタンクリック
+        /// ////////////////////////////////////////////////
+        int openflag = 0;
+        int restart = 0;
+
+        // startボタン
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            openflag = 0;
+            restart = 0;
+            try
+            {
+                statusLabel.Text = "ON";
+                if (restart == 0)
+                {
+                    serialPort.Close();
+                    serialPort.Open();
+                    openflag++;
+                    send_command("9A 13 00 00 01 01 00 00 00 00 00 01 01 00 00 00");//計測開始，終了時刻の設定
+                }
+                else if (restart != 0)
+                {
+                    send_command("9A 13 00 00 01 01 00 00 00 00 00 01 01 00 00 00");
+                    restart = 0;
+                    openflag++;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("センサの電源が入っているか確認してください．");
+            }
+        }
+
+        // stopボタン
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "OFF";
+            if(openflag == 0)
+            {
+                MessageBox.Show("接続されていません", "エラー");
+            }
+            else
+            {
+                send_command("9A 15 00");
+                openflag = 0;
+                restart++;
+            }
+        }
+
+
+        /// コマンド送信関連
+        /// ////////////////////////////////////////////////
+        // コマンド送信
+        private void send_command(string str)
+        {
+            string[] hexValueSplit = (get_bcc(str)).Split(' ');
+            byte[] binary = new byte[hexValueSplit.Length];
+
+            for (int i = 0; i < hexValueSplit.Length; i++)
+            {
+                binary[i] = Convert.ToByte(hexValueSplit[i], 16);
+            }
+            serialPort.Write(binary, 0, binary.Length);
+        }
+
+        // 16進文字列にbcc付加して返す
+        private string get_bcc(string str)
+        {
+            byte[] bcc = new byte[1];
+            bcc[0] = 0;
+            string[] value = str.Split(null as string[], 30, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                bcc[0] ^= (Byte)Convert.ToInt32(value[i], 16);
+            }
+            str += " " + BitConverter.ToString(bcc);
+            return str;
+        }
+
+        /// グラフの描画の関数
+        /// ////////////////////////////////////////////////
+        int t_count = 0;        // x座標カウント用
+        int DataMax = 50000;    // データ表示上限値
+        short pre_y1, pre_y2;       // データ保存用変数
+
+        // 筋電値のグラフ描画
+        void MakeGraphicsEMG(short y1, short y2)
         {
 
+            Graphics g1 = graph1.CreateGraphics();
+
+            Pen pen1 = new Pen(Color.DarkOrange, 1);
+            Pen pen2 = new Pen(Color.DarkBlue, 1);
+            Pen penA = new Pen(Color.Black, 1);
+
+            // 初期描画(y座標線)
+            if (t_count == 0)
+            {
+                g1.DrawLine(penA, 0, Ypoint(DataMax/ 2), graph1.Width, Ypoint(DataMax / 2));
+
+            }
+
+            // グラフ描画
+            if (t_count > 0)
+            {
+                g1.DrawLine(pen1, (t_count - 1) * 2, Ypoint(50000 / 2 + pre_y1 / 2), 2 * t_count, Ypoint(50000 / 2 + y1 / 2));
+                g1.DrawLine(pen2, (t_count - 1) * 2, Ypoint(50000 / 2 + pre_y2 / 2), 2 * t_count, Ypoint(50000 / 2 + y2 / 2));
+            }
+
+            // 過去データとして保存
+            pre_y1 = y1;
+            pre_y2 = y2;
+
+            // カウントアップ
+            t_count++;
+
+            // 終端処理
+            if (t_count == graph1.Width / 2)
+            {
+                graph1.Image = null;
+                t_count = 0;
+            }
+
+            //定義破棄
+            g1.Dispose();
+            pen1.Dispose(); pen2.Dispose();
+            penA.Dispose();
+        }
+
+        // a: 表示したいデータ値  Ypoint: 表示される座標
+        int Ypoint(int a)
+        {
+            return graph1.Height - (a / (DataMax / graph1.Height));
         }
     }
 }
